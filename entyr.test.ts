7a458a79d5dbe4228b4414f1378c31d16539ed48
Nitsan Avni <nitsanav@gmail.com>
2022-03-07 17:25:25 +0000

    cleaner

--- entyr.test.ts
+++ entyr.test.ts
@@ -3,7 +3,7 @@ import { $, nothrow } from "zx";
 
 test("sanity", async (t) => {
   t.regex(
-    (await $`man entr`).stdout,
+    (await $`man entr | head -n 4 | tail -n 1`).stdout,
     /run arbitrary commands when files change/
   );
 });
